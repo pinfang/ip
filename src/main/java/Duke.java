aef1994ea3b2254d1359746d6d952231c0bd4e31
pinfang <e0425430@u.nus.edu>
2020-08-19 21:21:10 +0800

    Level 0. Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,6 @@ package main.java;
 public class Duke {
 
     public static void simpleGreet() {
-        
         System.out.println("_________________________________________");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
@@ -13,13 +12,6 @@ public class Duke {
     }
 
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-
         simpleGreet();
     }
 }
