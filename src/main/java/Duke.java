081470b2fd4be9c6f81bed8a05e6d1b8717e8e4d
pinfang <e0425430@u.nus.edu>
2020-08-21 17:45:46 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,37 @@
 package main.java;
+import java.util.Scanner;
 
 public class Duke {
-    public static void simpleGreet() {
+    public static void greet() {
         System.out.println("_________________________________________");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+        System.out.println("_________________________________________");
+    }
+
+    public static void exit() {
         System.out.println("_________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("_________________________________________");
     }
 
+    public static void echo() {
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            String command = scan.nextLine();
+            if (command.equals("bye")) {
+                exit();
+                break;
+            } else {
+                System.out.println("_________________________________________");
+                System.out.println(command);
+                System.out.println("_________________________________________");
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        simpleGreet();
+        greet();
+        echo();
     }
 }
