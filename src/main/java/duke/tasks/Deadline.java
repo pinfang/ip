c585bdad5fe7137df25eea10c3f753e05ad72199
pinfang <e0425430@u.nus.edu>
2020-09-20 21:46:35 +0800

    able to convert date and time

--- src/main/java/duke/tasks/Deadline.java
+++ src/main/java/duke/tasks/Deadline.java
@@ -1,15 +1,37 @@
 package duke.tasks;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class Deadline extends Task {
-    protected String by;
+    private String date = "";
+    private String time = "";
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        String[] timings = by.trim().split("\\s");
+        this.date = timings[0];
+        this.time = timings[1];
     }
 
     @Override
     public String toString() {
-        return " [D]" + super.toString() + "(by:" + by + ")";
+        return " [D]" + super.toString() + "(by: " + date() + time() + ")";
+    }
+
+    private String date() {
+        LocalDate date = LocalDate.parse(this.date.trim());
+        String newDate = date.format(DateTimeFormatter
+                .ofPattern("MMM d yyyy").withLocale(Locale.ENGLISH));
+        return newDate;
+    }
+
+    private String time() {
+        LocalTime time = LocalTime.parse(this.time.trim(), DateTimeFormatter.ofPattern("HHmm"));
+        String newTime = time.format(DateTimeFormatter
+                .ofPattern("h.mm a").withLocale(Locale.ENGLISH));
+        return ", " + newTime;
     }
 }
--- src/main/java/duke/tasks/Event.java
+++ src/main/java/duke/tasks/Event.java
@@ -1,15 +1,37 @@
 package duke.tasks;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class Event extends Task {
-    protected String at;
+    private String date = "";
+    private String time = "";
 
     public Event(String description, String at) {
         super(description);
-        this.at = at;
+        String[] timings = at.trim().split("\\s");
+        date = timings[0];
+        time = timings[1];
     }
 
     @Override
     public String toString() {
-        return " [E]" + super.toString() + "(at:" + at + ")";
+        return " [E]" + super.toString() + "(at:" + date() + time() + ")";
+    }
+
+    private String date() {
+        LocalDate date = LocalDate.parse(this.date.trim());
+        String newDate = date.format(DateTimeFormatter
+                .ofPattern("MMM d yyyy").withLocale(Locale.ENGLISH));
+        return newDate;
+    }
+
+    private String time() {
+        LocalTime time = LocalTime.parse(this.time.trim(), DateTimeFormatter.ofPattern("HHmm"));
+        String newTime = time.format(DateTimeFormatter
+                .ofPattern("h.mm a").withLocale(Locale.ENGLISH));
+        return ", " + newTime;
     }
 }
